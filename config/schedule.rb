66f9291636b72efd4a2bbccebf19fae5c86b4dfa
Jag <jag@producthunt.co>
2023-02-27 18:35:45 +0530

    Adjust schedule.rb
    For Development purposes, run cron every minute
    * create partners for this month, re-creates partners
      only when you remove them manually in db

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,7 +7,17 @@
 #
 set :output, "log/cron_log.log"
 
-#Create mystery lunched every 1 of month.
-every 1.month, :at => "1 AM" do
+#
+# Uncomment/comment line:18 and line:20, to create partners every minute or once a month
+#
+# 1. Run in console to remove current month partners
+#    LunchGroup.where('lunch_date > ?', Date.today.beginning_of_month).destroy_all
+#    LunchPartner.where(month: :February).destroy_all
+# 2. update crontab
+#    whenever --update-crontab --set environment=development
+
+  every 1.minutes do
+#Create mystery lunches every 1 of month.
+# every 1.month, :at => "1 AM" do
   runner "LunchPartner.create_mystery_lunch_for_current_month"
 end
